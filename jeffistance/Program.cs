d38aee02d125f0e23983859c02a058a66089403b
Bancho <gnr11matos@gmail.com>
2020-01-12 23:06:25 +0000

    Added Project

---
+++ jeffistance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jeffistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
