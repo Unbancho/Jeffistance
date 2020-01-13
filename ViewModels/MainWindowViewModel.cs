7ab99a258672b0874ffb7bc499dc53bcd1fde3ed
Bancho <gnr11matos@gmail.com>
2020-01-13 04:36:37 +0000

    Button prints truth

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Avalonia.Controls;
+using Avalonia.Media.Imaging;
 
 namespace Jeffistance.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Casca gae";
-        public string TheTruth => "Casca super gae";
+        public string TheTruth => "Jeff ungae";
+    
+        public void OnClicked()
+        {
+            Console.WriteLine("casca gae");
+        }
     }
 }
