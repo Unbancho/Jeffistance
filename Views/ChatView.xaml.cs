9fb6de440fc4437dd122a0620fe1d889123da7d4
ZackUnfair <acezackfair@gmail.com>
2020-04-16 13:05:46 +0200

    Add auto focus to chat (#13)

--- Views/ChatView.xaml.cs
+++ Views/ChatView.xaml.cs
@@ -5,9 +5,12 @@ namespace Jeffistance.Views
 {
     public class ChatView : UserControl
     {
+        public TextBox MessageTextBox => this.FindControl<TextBox>("MessageTextBox");
+
         public ChatView()
         {
             this.InitializeComponent();
+            MessageTextBox.Initialized += (sender, args) => MessageTextBox.Focus();
         }
 
         private void InitializeComponent()
