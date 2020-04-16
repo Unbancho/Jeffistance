26c279d44b7f0b8fa261437125ef254aa6a4626c
ZackUnfair <acezackfair@gmail.com>
2020-04-16 12:56:11 +0200

    Add about window (#12)

--- ViewModels/MainMenuViewModel.cs
+++ ViewModels/MainMenuViewModel.cs
@@ -1,3 +1,9 @@
+using System.Reactive;
+using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.VisualTree;
+using ReactiveUI;
+
 namespace Jeffistance.ViewModels
 {
     public class MainMenuViewModel : ViewModelBase
@@ -6,9 +12,37 @@ namespace Jeffistance.ViewModels
         public string Greeting => "0.01";
         public string TheTruth => "Jeff ungae";
 
+        public ReactiveCommand<Control, Unit> AboutMenu { get; } 
+
         public MainMenuViewModel(MainWindowViewModel parent)
         {
             this.parent = parent;
+
+            AboutMenu = ReactiveCommand.Create<Control>(
+                (control) => {
+                    Window popupWindow = CreateAboutWindow();
+                    popupWindow.ShowDialog((Window)control.GetVisualRoot());
+                }
+            );
+        }
+
+        private Window CreateAboutWindow()
+        {
+            Window window = new Window()
+            {
+                Title = "About",
+                ShowInTaskbar = false,
+                Height = 200,
+                Width = 200,
+                WindowStartupLocation = WindowStartupLocation.CenterOwner
+            };
+            window.Content = new TextBlock {
+                Text = "Jeffistance is a very epic app made by epic people for epic people.",
+                TextWrapping = TextWrapping.Wrap,
+                FontSize = 24
+            };
+            
+            return window;
         }
 
         private void OnHostButtonClick()
