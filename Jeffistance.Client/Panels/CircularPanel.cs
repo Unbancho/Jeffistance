800e14db885b24478dcd644ca6f4872fce7c2659
Unbancho <badsilverwolf@gmail.com>
2020-05-25 23:24:44 +0100

    Add Processing-Power-Dependent Perfection to Ellipse Pattern

--- Jeffistance.Client/Panels/CircularPanel.cs
+++ Jeffistance.Client/Panels/CircularPanel.cs
@@ -1,38 +1,46 @@
 using Avalonia;
 using Avalonia.Controls;
 using System;
-using Jeffistance.Common.ExtensionMethods;
+using System.Collections.Generic;
+using Jeffistance.Common.JeffMath;
+using System.Linq;
+
 
 namespace Jeffistance.Client.ViewModels
 {
     public class CircularPanel : Panel
     {
-
-        private Point GetPoint(Point circleCenter, int childIndex, int childCount, Control child)
-        {
-            (double rx, double ry) = (circleCenter.X - child.DesiredSize.Width, circleCenter.Y - child.DesiredSize.Height);
-
-            //Dividing the angles for the number of players
-            double angle = (childIndex * 360) / childCount;
-
-            double xp2 = (circleCenter.X + rx * Math.Cos(angle.ToRadians()) - child.DesiredSize.Width/2);
-            double yp2 = (circleCenter.Y + ry * Math.Sin(angle.ToRadians()) - child.DesiredSize.Height/2);
-            
-            return new Point(xp2, yp2);
-        }
-
-
         protected override Size ArrangeOverride(Size finalSize)
         {
             Point circleCenter = new Point(finalSize.Width/2, finalSize.Height/2);
-            int childIndex = 0;
-            foreach (Control child in Children)
+            List<Point> Placements = CalculatePlacements(Children.Count, 400, 150);
+            foreach (var (placement, child) in Placements.Zip(Children, (placement, child) => (placement, child)))
             {
-                Point childPoint = GetPoint(circleCenter, childIndex, Children.Count, child);
-                child.Arrange(new Rect(childPoint, child.DesiredSize));
-                childIndex++;
+                child.Arrange(new Rect(circleCenter + new Point(placement.X, placement.Y) - new Point(child.DesiredSize.Width/2, child.DesiredSize.Height/2), child.DesiredSize));
             }
             return finalSize; // Returns the final Arranged size
+        }
+
+        private List<Point> CalculatePlacements(int nPlacements, double a=1, double b=1, double precision=3)
+        {
+            List<Point> placements = new List<Point>();
+            double angle = 0;
+            double deltaAngle = Math.Pow(10, -precision);
+            double nIntegrals = Math.Round(2*Math.PI/deltaAngle);
+            int nextPoint = 0;
+            double run = 0;
+            double circumference = JeffMath.CalculateEllipseCircumference(a, b);
+            for( int i=0; i < nIntegrals; i++ ) {
+                angle += deltaAngle;
+                double subIntegral = nPlacements*run/circumference;
+                if( (int) subIntegral >= nextPoint ) 
+                {
+                    placements.Add(new Point(a * Math.Cos(angle), b * Math.Sin(angle)));
+                    nextPoint++;
+                }
+                run += Math.Sqrt(Math.Pow(a*Math.Sin(angle), 2) + Math.Pow(b*Math.Cos(angle), 2));
+            }
+            return placements;
         }
     }
 }
